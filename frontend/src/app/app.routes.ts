35c7364ecfbf252d5fe46dbd57a7cbaaa19f0a0a
Zeno França <zenofranca@Zenos-MacBook-Air.local>
2023-09-03 11:35:31 -0300

    stable updated version

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+    path: '',
+    title: 'Home Site',
+    component: NxWelcomeComponent
+}];
